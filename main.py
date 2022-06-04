ff8614ab489009ac4179bfdb03fefb1b9442b387
Narek <93737445+17N71@users.noreply.github.com>
2022-06-04 04:27:44 +0400

    4.5.6

---
+++ main.py
@@ -0,0 +1,26 @@
+# def tbonb(a,b):
+#     if type(a) == type(b):
+#         print(type(a) == type(b))
+#     else:
+#         print(type(a) == type(b))
+# tbonb(1,"r")
+# 4 homewrok
+# listBihThenSix = []
+# def sixBiggers(sb = []):
+#     for k in sb:
+#         if k >= 6:
+#             listBihThenSix.append(k)
+#     print(sum(listBihThenSix))
+# sixBiggers([5,6,9,7,8,15])
+# 5 homework
+
+
+# NewList = []
+# def sss(b,e = []):
+#     for i in e:
+#         if b < i:
+#          NewList.append(i)
+#     print("it is list with numbers whose bigger than: ",NewList )
+#     print("the sum of list: ",sum(NewList))
+# sss(5,[1,2,5,9,6,10,3,15])
+# 6 homework
